c9add144a86c8dc3501d5403ea25a6b4ae7994f4
Vallepu veerendra kumar <vallepu.veeru@gmail.com>
2014-05-03 14:22:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function MyCtrl($scope,$filter){
+var app = angular.module('app', []);
+app.controller('MyCtrl',function($scope,$filter){
     $scope.inputStr="Demo";
     $scope.test;
     $scope.ToUpper=function(changeCase){
     $scope.test=$filter(changeCase)($scope.inputStr);
     };
-}
+});
